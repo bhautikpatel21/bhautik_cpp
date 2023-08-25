446cd8b6e517e10f1e1d9d540e19a1333b6bd195
bdudhat293@gmail.com <bdudhat293@gmail.com>
2023-08-25 08:11:42 +0530

    cpp

---
+++ continue3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int i,j;
+
+    for (i=1; i<=5; i++)
+    {
+        for (j=1; j<=i; j++)
+       {
+        if (i==2 || i==1)
+        {
+            continue;
+        }
+        cout << i;
+       }
+       cout <<"\n";
+    }
+}
