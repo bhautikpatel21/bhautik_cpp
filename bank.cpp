c282478a5616c141fa15fcc4e782979c7c87eebc
bdudhat293@gmail.com <bdudhat293@gmail.com>
2023-09-18 09:24:14 +0530

    18-09-2023

---
+++ bank.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+class bank
+{
+    int bankbalance ,wibankbalance;
+    int deposite,withdrow;
+    int total,witotal;
+
+    public :
+
+    void depbalance ()
+    {
+        cout << "Enter your bankbalance : ";
+        cin >> bankbalance;
+        cout << "Enter the deposite : ";
+        cin >> deposite;
+        total=bankbalance+deposite;
+        cout << "Total balance : ";
+        cout << total << endl;
+    }
+
+    void wibalance ()
+    {
+        cout << "Enter the bankbalance : ";
+        cin >> wibankbalance;
+        cout<< "Enter the withdrow : ";
+        cin >> withdrow;
+        witotal=wibankbalance-withdrow;
+        cout << "total bankbalance : " ;
+        cout << witotal << endl;
+    }
+
+    void showall ()
+    {
+        cout << "Bank balance : " << bankbalance << endl;
+    }
+};
+
+int main()
+{    
+     bank ba;
+
+    int num; 
+
+    cout << "Enter the number : ";
+    cin >> num;
+
+    for (int i=1; i<=3; i++)
+
+    if (num==1)
+    {
+        cout << "deposite\n";
+        ba.depbalance ();
+    }
+    else if (num==2)
+    {
+        cout << "wibalance\n";
+        ba.wibalance ();
+    }
+    else if (num==3)
+    {
+        cout << "show datail\n";
+        ba.showall();
+    }
+}
--- maths.cpp
+++ maths.cpp
@@ -8,18 +8,23 @@ int main()
     cout << "ceil number : " <<ceil(3.5)<<endl;
     cout << "ceil nunber : " <<ceil(3.1)<<endl;
     cout << "ceil number : " <<ceil(3.5)<<endl;
+
     cout << "floor number :" <<floor(5.3)<<endl;
     cout << "floor number :" <<floor(5.5)<<endl;
     cout << "floor number :" <<floor(5.9)<<endl;
+
     cout << "round number : "<<round(8.9)<<endl;
     cout << "round number : "<<round(8.5)<<endl;
     cout << "round number : "<<round(8.2)<<endl;
+
     cout << "sqrt number : "<<sqrt(16)<<endl;
     cout << "sqrt number : "<<sqrt(4)<<endl;
     cout << "sqrt number : "<<sqrt(25)<<endl;
+
     cout << "power nuumber :"<<pow(4,2)<<endl;
     cout << "power nuumber :"<<pow(8,3)<<endl;
     cout << "power nuumber :"<<pow(2,6)<<endl;
+    
     cout << "abs number : "<<abs(-8)<<endl;
     cout << "abs number : "<<abs(-15)<<endl;
     cout << "abs number : "<<abs(21)<<endl;
